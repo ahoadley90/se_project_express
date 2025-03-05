9ebf73b88fe157338560988fe46fafea00c024ee
AmandaKim <akimmets@gmail.com>
2025-03-05 08:59:45 -0600

    correct routes and errors file

--- errors/index.js
+++ errors/index.js
@@ -2,10 +2,14 @@ const NotFoundError = require("./NotFoundError");
 const ForbiddenError = require("./ForbiddenError");
 const BadRequestError = require("./BadRequestError");
 const InternalServerError = require("./InternalServerError");
+const ConflictError = require("./ConflictError");
+const UnauthorizedError = require("./UnauthorizedError");
 
 module.exports = {
   NotFoundError,
   ForbiddenError,
   BadRequestError,
   InternalServerError,
+  ConflictError,
+  UnauthorizedError,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const clothingItemsRoutes = require("./clothingItems");
 const userRoutes = require("./users");
+const { createUser, login } = require("../controllers/users");
 
+// Public routes
+router.post("/signup", createUser);
+router.post("/signin", login);
+
+// Protected routes
 router.use("/items", clothingItemsRoutes);
 router.use("/users", userRoutes);
 
