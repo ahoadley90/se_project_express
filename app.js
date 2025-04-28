8aa90a9db1cb1c84e2e2c80eb1c04928d3a6e286
AmandaKim <akimmets@gmail.com>
2025-04-27 20:38:57 -0500

    update logger file to reflect error logging

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { errors } = require("celebrate");
 const routes = require("./routes");
 const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -16,12 +17,16 @@ mongoose.connect("mongodb://localhost:27017/wtwr_db", {
 app.use(cors());
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.get("/ping", (req, res) => {
   res.status(200).json({ message: "Server is alive" });
 });
 
 // your routes
 app.use(routes);
+
+app.use(errorLogger);
 
 // Celebrate error handler
 app.use(errors());
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,28 +1,38 @@
 const winston = require("winston");
 const expressWinston = require("express-winston");
 
+const messageFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.printf(
+    ({ level, message, meta, timestamp }) =>
+      `${timestamp} ${level}: ${meta.error?.stack || message}`
+  )
+);
+
 // Create a request logger
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "request.log" }),
+    new winston.transports.Console({
+      format: messageFormat,
+    }),
+    new winston.transports.File({
+      filename: "request.log",
+      format: winston.format.json(),
+    }),
   ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
 });
 
 // Create an error logger
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "error.log" }),
+    new winston.transports.Console({
+      format: messageFormat,
+    }),
+    new winston.transports.File({
+      filename: "error.log",
+      format: winston.format.json(),
+    }),
   ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
 });
 
 module.exports = {
