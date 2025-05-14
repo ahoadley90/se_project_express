2fc607d9f842ff5e4d848333f1c02e5f39a870e7
AmandaKim <akimmets@gmail.com>
2025-05-14 10:19:37 -0500

    simplify mongoose connection

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ console.log(
 );
 
 //prettier-ignore
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
